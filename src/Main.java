78b5034e096f8344969804fabd19686f284f3cc0
AndrewBy5 <55550052+AndrewBy5@users.noreply.github.com>
2020-03-16 12:02:03 +0300

    First commit

--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,28 @@
 public class Main {
     public static void main(String[] args) {
 
-        /* TaskTwo order = new TaskTwo();
+        /*TaskTwo order = new TaskTwo();
         order.playWithMisterX();
         order.orderLinesOfNumbers();
         order.averageOfNumbers();*/
 
 
-        TaskOne printAll = new TaskOne();
+        /*TaskOne printAll = new TaskOne();
         //printAll.methodWhileOne();
-
-        // TaskOne oddNumberOutput = new TaskOne();
-        //oddNumberOutput.methodWhileTwo();
-        printAll.methodWhileTwo();
-
-        printAll.methodWhileThree();
-
+        //printAll.methodWhileTwo();
+        //printAll.methodWhileThree();
         printAll.methodWhileFour();
 
-
-
-
-
-
-
-
-
+        printAll.methodForOne();
+        printAll.methodForTwo();
+        printAll.methodForThree();
+        printAll.methodForFour();*/
 
 
 
 
     }
 
-    }
 
+
+}
--- src/TaskOne.java
+++ src/TaskOne.java
@@ -2,7 +2,7 @@ public class TaskOne{
 
     /*public void methodWhileOne() {
         int a = 1000;
-        int i = 0;
+        //? int i = 0;
         while (a < 10000) {
             System.out.println(a);
             a += 3;
@@ -40,4 +40,32 @@ public class TaskOne{
         System.out.println();
     }
 
+    public void methodForOne(){
+        System.out.println(" From 10000 to 100000");
+        for (int a = 1000; a < 10000; a+=3){
+            System.out.print(a + " ");
+        }
+        System.out.println();
+    }
+    public void methodForTwo(){
+        System.out.println("55 циклов");
+        for (int b =1, z = 0; z < 55; z ++, b +=2 ){
+            System.out.print(b + " " );
+        }
+        System.out.println();
+    }
+    public void methodForThree(){
+        System.out.println("Name");
+        for(int c=90; c > -1; c-=5){
+            System.out.print(c + " ");
+        }
+        System.out.println();
+    }
+    public void methodForFour(){
+        System.out.println("Name");
+        for (int d = 2, x = 0; x < 20 ; d *= 2, x++){
+            System.out.print(d + " ");
+        }
+        System.out.println();
+    }
 }
